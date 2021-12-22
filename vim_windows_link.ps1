ebb307f0b8d488e6e0cd42095d98183ae4f96c06
Carl <Carl.Kamholtz@gmail.com>
2021-12-23 07:38:08 +1000

    update windows link

--- vim_windows_link.ps1
+++ vim_windows_link.ps1
@@ -1,11 +1,13 @@
-Write-Output "Current dir: " $PSScriptRoot
-cmd /c mklink $HOME"\AppData\Local\nvim\init.vim" $PSScriptRoot"\.vimrc"
-cmd /c mklink $HOME"\AppData\Local\nvim\coc-settings.json" $PSScriptRoot"\coc-settings.json"
+# Write-Output "Current dir: " $PSScriptRoot
+# cmd /c mklink $HOME"\AppData\Local\nvim\init.vim" $PSScriptRoot"\.vimrc"
+# cmd /c mklink $HOME"\AppData\Local\nvim\coc-settings.json" $PSScriptRoot"\coc-settings.json"
 
-Write-Output $env:APPDATA"\..\Local\nvim\fnl"
+# Write-Output $env:APPDATA"\..\Local\nvim\fnl"
 
-cmd /c mklink /D $env:APPDATA"\..\Local\nvim\fnl" $PSScriptRoot"\fnl"
+# cmd /c mklink /D $env:APPDATA"\..\Local\nvim\fnl" $PSScriptRoot"\fnl"
 
-cmd /c mklink $HOME"\.vimrc" $PSScriptRoot"\.vimrc"
+# cmd /c mklink $HOME"\.vimrc" $PSScriptRoot"\.vimrc"
 
-cmd /c mklink $HOME"\.vsvimrc" $PSScriptRoot"\.vsvimrc"
+# cmd /c mklink $HOME"\.vsvimrc" $PSScriptRoot"\.vsvimrc"
+
+cmd /c mklink /D $env:APPDATA"\..\Local\nvim\" $PSScriptRoot"\.config\nvim"
